9005740c18fc350fa4af7870cb79230dc10b6409
dcolombo <dcolombo@corp.amazonworkspaces.com>
2023-01-30 07:52:04 -0500

    working on new registration component

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,22 +1,22 @@
 export type AmplifyDependentResourcesAttributes = {
-    "analytics": {
-        "patemobile": {
-            "Region": "string",
-            "Id": "string",
-            "appName": "string"
-        }
-    },
-    "storage": {
-        "s3eorimages2022": {
-            "BucketName": "string",
-            "Region": "string"
-        }
-    },
-    "api": {
-        "patemobile": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
+  "analytics": {
+    "patemobile": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
     }
+  },
+  "api": {
+    "patemobile": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "storage": {
+    "s3eorimages2022": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
 }
